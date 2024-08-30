58fd30cecc8b9b4f62ed64add41cc72161980444
MSI <chathurabhashitha01@gmail.com>
2024-08-30 19:56:40 +0530

    home page creation

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import RainImg from '../../images/rain.png';
+
+export function HomePage() {
+    return (
+        <div className="relative h-[100vh] w-full bg-[url('C:\react\wether_forecast\src\images\rain.png')] bg-cover bg-center">
+            <div className='w-full h-64 bg-black opacity-[50%]'>
+
+            </div>
+            
+        </div>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Jost', 'sans']
+      },
+      colors: {
+        primary: '#FFFFFF',
+        secondary: '#000000'
+      }
+    },
+  },
+  plugins: [],
+}
